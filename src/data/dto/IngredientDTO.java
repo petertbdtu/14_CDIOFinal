37fe2ed5e99210667bebdcfc00cb4670fd0210af
petertbdtu <s175210@student.dtu.dk>
2018-06-02 23:03:12 +0200

    Filled in IngredientDTO

--- src/data/dto/IngredientDTO.java
+++ src/data/dto/IngredientDTO.java
@@ -1,5 +1,37 @@
 package dto;
 
 public class IngredientDTO {
-
+	
+	private int id;
+	private String name;
+	private double amount;
+	
+	public IngredientDTO() {
+		//For Jackson
+	}
+	
+	public IngredientDTO(int id, String name, double amount) {
+		this.id = id;
+		this.name = name;
+		this.amount = amount;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
 }
