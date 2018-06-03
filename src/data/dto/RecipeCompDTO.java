53f0f0adc9eed668574f4e41590db939914c5ec2
Kasper <Kasper@DESKTOP-HDGPRFP.home>
2018-06-03 16:07:22 +0200

    DTO for recipe components (ingredient, amount and tolerance) Dunno
    if the correct datatype is used for tolerance.

--- src/data/dto/RecipeCompDTO.java
+++ src/data/dto/RecipeCompDTO.java
@@ -1,5 +1,40 @@
-package dto;
+package data.dto;
 
 public class RecipeCompDTO {
+	String ingredient;
+	int amount;
+	int tolerance;
+	
 
+	public RecipeCompDTO(String ingredient, int amount, int tolerance) {
+		this.ingredient = ingredient;
+		this.amount = amount;
+		this.tolerance = tolerance;
+	}
+
+	public String getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(String ingredient) {
+		this.ingredient = ingredient;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public int getTolerance() {
+		return tolerance;
+	}
+
+	public void setTolerance(int tolerance) {
+		this.tolerance = tolerance;
+	}
+	
+	
 }
