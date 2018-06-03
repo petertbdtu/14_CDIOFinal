5e72cbdc11e80e6e5915b12be25ad65dcad1eab5
Mette s <s172840@student.dtu.dk>
2018-06-03 22:42:01 +0200

    addad emty constructor + attributes + getter&setter

--- src/data/dto/IngredientBatchDTO.java
+++ src/data/dto/IngredientBatchDTO.java
@@ -1,5 +1,35 @@
-package dto;
+package data.dto;
 
-public class IngredientBatchDTO {
+import data.dao.*;
 
+import data.idao.*;
+
+
+public class IngredientBatchDTO 
+{
+	private int ibID;
+	private int ingbatchID;
+	double amount;
+	public IngredientBatchDTO() {
+
+	}
+
+public int getIbID() {
+	return ibID;
+}
+public void setIbID(int ibID) {
+	this.ibID = ibID;
+}
+public int getIngbatchID() {
+	return ingbatchID;
+}
+public void setIngbatchID(int ingbatchID) {
+	this.ingbatchID = ingbatchID;
+}
+public double getAmount() {
+	return amount;
+}
+public void setAmount(double amount) {
+	this.amount = amount;
+}
 }
