d5d9e6a418e7f213230f8332b5e3fec237366fbc
Thomas <Thomas@DESKTOP-UFIIPH8>
2018-06-03 20:12:10 +0200

    Added empty constructor, getters, and setters for all variables

--- src/data/dto/ProductBatchCompDTO.java
+++ src/data/dto/ProductBatchCompDTO.java
@@ -1,5 +1,47 @@
 package dto;
 
-public class ProductBatchCompDTO {
+
+public class ProductBatchCompDTO 
+{
+	private int prodBatchID;
+	private int ingbatchID;
+	private double tara;
+	private double netto;
+	private int usrID;
+	public ProductBatchCompDTO() 
+	{
+
+	}
+	public int getProdBatchID() {
+		return prodBatchID;
+	}
+	public void setProdBatchID(int prodBatchID) {
+		this.prodBatchID = prodBatchID;
+	}
+	public int getIngbatchID() {
+		return ingbatchID;
+	}
+	public void setIngbatchID(int ingbatchID) {
+		this.ingbatchID = ingbatchID;
+	}
+	public double getTara() {
+		return tara;
+	}
+	public void setTara(double tara) {
+		this.tara = tara;
+	}
+	public double getNetto() {
+		return netto;
+	}
+	public void setNetto(double netto) {
+		this.netto = netto;
+	}
+	public int getUsrID() {
+		return usrID;
+	}
+	public void setUsrID(int usrID) {
+		this.usrID = usrID;
+	}
+
 
 }
