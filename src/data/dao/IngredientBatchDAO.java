b3c319a035ca5a729932b11143614a810815bb5d
petertbdtu <s175210@student.dtu.dk>
2018-06-04 11:20:43 +0200

    Filled in IngredientBatchDTO, added supplier to DTO

--- src/data/dao/IngredientBatchDAO.java
+++ src/data/dao/IngredientBatchDAO.java
@@ -1,7 +1,67 @@
 package data.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import data.dto.IngredientBatchDTO;
 import data.idao.IIngredientBatchDAO;
 
 public class IngredientBatchDAO implements IIngredientBatchDAO {
+	
+	private static IngredientBatchDAO instance = new IngredientBatchDAO();
+	private List<IngredientBatchDTO> ingredientBatches;
+	
+	private IngredientBatchDAO()
+	{
+		// Load from file
+		ingredientBatches = new ArrayList<IngredientBatchDTO>();
+	}
+	
+	public static IngredientBatchDAO getInstance()
+	{
+		return instance;
+	}
+	
+	@Override
+	public IngredientBatchDTO getIngredientBatch(int ibId) {
+		IngredientBatchDTO ingbatch = ingredientBatches.get(ibId);
+		if (ingbatch != null)
+			return ingredientBatches.get(ibId);
+		else
+			throw new DALException("Ingredient with this ID does not exist.");
+	}
+
+	@Override
+	public List<IngredientBatchDTO> getIngredientBatchList() {
+		return ingredientBatches;
+	}
+
+	@Override
+	public List<IngredientBatchDTO> getIngredientBatchList(int ingredientId) {
+		List<IngredientBatchDTO> batchesOfIngredient = new ArrayList<IngredientBatchDTO>();
+		for (IngredientBatchDTO ingbatch : ingredientBatches)
+		{
+			if (ingbatch.getIbID() == ingredientId)
+				batchesOfIngredient.add(ingbatch);
+		}
+		return batchesOfIngredient;
+	}
+
+	@Override
+	public void createIngredientBatch(IngredientBatchDTO ingredientBatch) throws DALException {
+		if (!ingredientBatches.contains(ingredientBatch))
+			ingredientBatches.add(ingredientBatch.getIbID(), ingredientBatch);
+		else
+			throw new DALException("Ingredient with this ID already exists.");
+	}
+
+	@Override
+	public void updateIngredientBatch(IngredientBatchDTO ingredientBatch) throws DALException {
+		if (!ingredientBatches.contains(ingredientBatch))
+			ingredientBatches.add(ingredientBatch.getIbID(), ingredientBatch);
+		else
+			throw new DALException("Ingredient with this ID does not exist.");
+		
+	}
 
 }
--- src/data/dto/IngredientBatchDTO.java
+++ src/data/dto/IngredientBatchDTO.java
@@ -1,35 +1,44 @@
 package data.dto;
 
-import data.dao.*;
-
-import data.idao.*;
-
-
-public class IngredientBatchDTO 
-{
+public class IngredientBatchDTO {
 	private int ibID;
 	private int ingbatchID;
-	double amount;
+	private double amount;
+	private String supplier;
+
 	public IngredientBatchDTO() {
 
 	}
 
-public int getIbID() {
-	return ibID;
-}
-public void setIbID(int ibID) {
-	this.ibID = ibID;
-}
-public int getIngbatchID() {
-	return ingbatchID;
-}
-public void setIngbatchID(int ingbatchID) {
-	this.ingbatchID = ingbatchID;
-}
-public double getAmount() {
-	return amount;
-}
-public void setAmount(double amount) {
-	this.amount = amount;
-}
+	public int getIbID() {
+		return ibID;
+	}
+
+	public void setIbID(int ibID) {
+		this.ibID = ibID;
+	}
+
+	public int getIngbatchID() {
+		return ingbatchID;
+	}
+
+	public void setIngbatchID(int ingbatchID) {
+		this.ingbatchID = ingbatchID;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public String getSupplier() {
+		return supplier;
+	}
+
+	public void setSupplier(String supplier) {
+		this.supplier = supplier;
+	}
 }
