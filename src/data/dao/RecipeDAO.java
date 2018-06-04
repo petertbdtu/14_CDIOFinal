4c83283f680a78ba8260ff4c3e2f1d85090d0b88
fagnig <fagnig@autistklassen.dk>
2018-06-04 10:11:19 +0200

    recipeDAO

--- src/data/dao/RecipeDAO.java
+++ src/data/dao/RecipeDAO.java
@@ -1,7 +1,57 @@
 package data.dao;
 
+import data.dto.RecipeDTO;
 import data.idao.IRecipeDAO;
 
-public class RecipeDAO implements IRecipeDAO{
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+public class RecipeDAO implements IRecipeDAO{
+    List<RecipeDTO> list;
+
+    public RecipeDAO() {
+        list = new ArrayList<>();
+    }
+
+    @Override
+    public RecipeDTO getRecipe(int recipeId) {
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()) {
+            RecipeDTO tempRecipe = (RecipeDTO) iterator.next();
+            if (tempRecipe.getRecipeId() == recipeId) {
+                return tempRecipe;
+            }
+        }
+
+        return null;
+        //Klar til at throwe exception
+        //throw new DALException("Fejl: Recept ikke fundet!");
+    }
+
+    @Override
+    public List<RecipeDTO> getRecipeList() {
+        return list;
+    }
+
+    @Override
+    public void createRecipe(RecipeDTO recipe) {
+        if(getRecipe(recipe.getRecipeId()) == null ) {
+            RecipeDTO tempRecipe = new RecipeDTO();
+
+            tempRecipe.setRecipeId(recipe.getRecipeId());
+            tempRecipe.setRecipeName(recipe.getRecipeName());
+
+            list.add(tempRecipe);
+        }
+    }
+
+    @Override
+    public void updateRecipe(RecipeDTO recipe) {
+        RecipeDTO curRecipe = getRecipe(recipe.getRecipeId());
+
+        if(curRecipe != null){
+            curRecipe.setRecipeName(recipe.getRecipeName());
+        }
+    }
 }
