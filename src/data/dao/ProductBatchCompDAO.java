1c12727eff979d57bab6ebf5e67af082508f217c
Thomas Vestergaard <Lattergassen@gmail.com>
2018-06-04 12:48:35 +0200

    fixed a stray bracket fucking up the code

--- src/data/dao/ProductBatchCompDAO.java
+++ src/data/dao/ProductBatchCompDAO.java
@@ -22,47 +22,46 @@ public class ProductBatchCompDAO implements IProductBatchCompDAO {
 				throw new DALException("No product batch with this ID exists.");
 			}
 	}
-}
 
-@Override
-public List<ProductBatchCompDTO> getProductBatchCompList(int pbId) {
-	List<ProductBatchCompDTO> prodbatch = new ArrayList<ProductBatchCompDTO>();
-	for (ProductBatchCompDTO complist : prodbatch)
-	{
-		if (complist.getpbID() == pbId) {
-			prodbatch.add(complist);
+	@Override
+	public List<ProductBatchCompDTO> getProductBatchCompList(int pbId) {
+		List<ProductBatchCompDTO> prodbatch = new ArrayList<ProductBatchCompDTO>();
+		for (ProductBatchCompDTO complist : prodbatch)
+		{
+			if (complist.getpbID() == pbId) {
+				prodbatch.add(complist);
+			}
+			else {
+				throw new DALException("No product batch list containing this ID exists.");
+			}
+		}
+		return prodbatch;
+
+	}
+
+	@Override
+	public List<ProductBatchCompDTO> getProductBatchCompList() {
+		return complist;
+	}
+
+	@Override
+	public void createProductBatchComp(ProductBatchCompDTO productBatchComp) {
+		if(!complist.contains(productBatchComp)) {
+			complist.add(productBatchComp.getpbID(), productBatchComp);
 		}
 		else {
-			throw new DALException("No product batch list containing this ID exists.");
+			throw new DALException("A product batch with this ID already exists.");
+		}
+
+	}
+
+	@Override
+	public void updateProductBatchComp(ProductBatchCompDTO productBatchComp) {
+		if(complist.contains(productBatchComp))
+			complist.add(productBatchComp);
+		else {
+			throw new DALException("No product batch with this ID exists.");
 		}
 	}
-	return prodbatch;
-
-}
-
-@Override
-public List<ProductBatchCompDTO> getProductBatchCompList() {
-	return complist;
-}
-
-@Override
-public void createProductBatchComp(ProductBatchCompDTO productBatchComp) {
-	if(!complist.contains(productBatchComp)) {
-		complist.add(productBatchComp.getpbID(), productBatchComp);
-	}
-	else {
-		throw new DALException("A product batch with this ID already exists.");
-	}
-
-}
-
-@Override
-public void updateProductBatchComp(ProductBatchCompDTO productBatchComp) {
-	if(complist.contains(productBatchComp))
-		complist.add(productBatchComp);
-	else {
-		throw new DALException("No product batch with this ID exists.");
-	}
-}
 
 }
