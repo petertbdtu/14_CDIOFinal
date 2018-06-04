c43f1826e73e02ecd1d2abbfa53dd541b44deb63
NK <NK@192.168.2.100>
2018-06-04 14:26:55 +0200

    Product Batch done

--- src/data/dao/ProductBatchDAO.java
+++ src/data/dao/ProductBatchDAO.java
@@ -11,7 +11,6 @@ public class ProductBatchDAO extends StorageDAO implements IProductBatchDAO {
 	
 	private static ProductBatchDAO instance = new ProductBatchDAO();
 	
-	
 	private ProductBatchDAO() {
 		try {
 			Map<Integer,ProductBatchDTO> productBatches = (Map<Integer, ProductBatchDTO>) super.load();
@@ -20,42 +19,59 @@ public class ProductBatchDAO extends StorageDAO implements IProductBatchDAO {
 		}
 	}
 	
-	
 	public static ProductBatchDAO getInstance() {
 		return instance;
 	}
 	
-	
 	@Override
 	public ProductBatchDTO getProductBatch(int pbId) {
-		Map<Integer,ProductBatchDTO> productBatches = (Map<Integer, ProductBatchDTO>) super.load();
-		return productBatches.get(pbId);
+		try {
+			Map<Integer, ProductBatchDTO> productBatches = (Map<Integer, ProductBatchDTO>) super.load();
+			return productBatches.get(pbId);
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	
 	@Override
 	public List<ProductBatchDTO> getProductBatchList() {
-		Map<Integer,ProductBatchDTO> productBatches = (Map<Integer, ProductBatchDTO>) super.load();
-		return (List<ProductBatchDTO>) productBatches.values();
+		try {
+			Map<Integer, ProductBatchDTO> productBatches = (Map<Integer, ProductBatchDTO>) super.load();
+			return (List<ProductBatchDTO>) productBatches.values();
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
-	
 	@Override
 	public void createProductBatch(ProductBatchDTO productBatch) {
-		Map<Integer,ProductBatchDTO> productBatches = (Map<Integer, ProductBatchDTO>) super.load();
-		if(!productBatches.containsKey(productBatch.getPbNr())) {
-			productBatches.put(productBatch.getPbNr(), productBatch);
-			super.save(productBatches);
+		try {
+			Map<Integer, ProductBatchDTO> productBatches = (Map<Integer, ProductBatchDTO>) super.load();
+			if(!productBatches.containsKey(productBatch.getPbNr())) {
+				productBatches.put(productBatch.getPbNr(), productBatch);
+				super.save(productBatches);
+			}
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
 	}
 
-	
 	@Override
 	public void updateProductBatch(ProductBatchDTO productBatch) {
-		Map<Integer,ProductBatchDTO> productBatches = (Map<Integer, ProductBatchDTO>) super.load();
-		productBatches.replace(productBatch.getPbNr(), productBatch);
-		super.save(productBatches);
+		try {
+			Map<Integer, ProductBatchDTO> productBatches = (Map<Integer, ProductBatchDTO>) super.load();
+			productBatches.replace(productBatch.getPbNr(), productBatch);
+			super.save(productBatches);
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/data/dto/ProductBatchDTO.java
+++ src/data/dto/ProductBatchDTO.java
@@ -1,40 +1,13 @@
 package data.dto;
 
-import java.util.ArrayList;
-
 public class ProductBatchDTO {
 	private int pbNr;//product batch Nr.
-	private int recept;//product recept
-	private String date;
+	private int receptId;//product recept
 	private String status;
-	private ArrayList<Integer> ingID = new ArrayList<Integer>();
-	private ArrayList<Integer> ingbID = new ArrayList<Integer>();
-	private ArrayList<Double> amount = new ArrayList<Double>();
 	
 	public ProductBatchDTO() {
 		
 	}
-	
-
-	public ArrayList<Integer> getIngbID() {
-		return ingbID;
-	}
-
-
-	public void setIngbID(ArrayList<Integer> ingbID) {
-		this.ingbID = ingbID;
-	}
-
-
-	public void setIngID(ArrayList<Integer> ingID) {
-		this.ingID = ingID;
-	}
-
-
-	public void setAmount(ArrayList<Double> amount) {
-		this.amount = amount;
-	}
-
 
 	public int getPbNr() {
 		return pbNr;
@@ -45,19 +18,11 @@ public class ProductBatchDTO {
 	}
 
 	public int getRecept() {
-		return recept;
+		return receptId;
 	}
 
 	public void setRecept(int recept) {
-		this.recept = recept;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
+		this.receptId = recept;
 	}
 
 	public String getStatus() {
@@ -67,21 +32,4 @@ public class ProductBatchDTO {
 	public void setStatus(String status) {
 		this.status = status;
 	}
-
-	public double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-
-	public String getIngID() {
-		return ingID;
-	}
-
-	public void setIngID(String ingID) {
-		this.ingID = ingID;
-	}
-	
 }
