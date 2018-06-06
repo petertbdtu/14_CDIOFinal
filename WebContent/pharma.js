53b0e823a219bdb329064df9e695d1ce2086a759
Thomas Vestergaard <lattergassen@gmail.com>
2018-06-06 16:20:45 +0200

    RESTed all of the recipe stuff, but it doesn't work :(((((

--- WebContent/pharma.js
+++ WebContent/pharma.js
@@ -7,10 +7,15 @@ function createComp() {
 	Ingredient = document.getElementById("Ingredient");
 	Amount = document.getElementById("Amount");
 	CreateIngredient = '<select id=commodityRecipe> <option>' + Ingredient + '<option> råvare 1 <option> råvare 2 <option> råvare 3 </select> <label for="amountOfCommodities">Antal</label> <input type="text" id="amountOfCommodities" placeholder="' + Amount + '"> <button type="button" onclick="createComp()">Tilføj råvare</button> <br><br/>';
-	document.getElementByID("IngredientForm").innerHTML += CreateIngredientForm;
+	document.getElementByID("123").innerHTML += CreateIngredientForm;
 	alert(Ingredient);
 }
 
 function createRecipe(ID) {
 var ID = $("#recipeID").val();
+}
+
+
+function createRecipe() {
+	
 }
