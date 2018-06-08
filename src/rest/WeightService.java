ff9def1a93b76141d3be6e3afbf8078ecff653f8
petertbdtu <s175210@student.dtu.dk>
2018-06-08 16:48:48 +0200

    Make startWeight service a POST, fix JS in laborant.html

--- src/rest/WeightService.java
+++ src/rest/WeightService.java
@@ -1,25 +1,29 @@
 package rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
 import ase.runnableThread;
 
 @Path("WeightService")
 public class WeightService {
 
-	@GET
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response startWeight() {
-		if(Thread.currentThread().isInterrupted() == true) {
+		if(Thread.currentThread().isAlive() == false) {
 			Thread thread = new Thread(new runnableThread());
 			thread.start();
 			return Response.status(200).build();
-		} else {
-			return Response.status(500).build();
 		}
+		
+		return Response.status(500).build();
 	}
 
 	@GET
